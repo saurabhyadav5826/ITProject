4caf0c964ce6117aff588ea5811cb11eb4e989a5
Saurabh Yadav <38484871+saurabhyadav5826@users.noreply.github.com>
2018-04-18 10:02:45 +0530

    Create candidate.php

---
+++ candidate.php
@@ -0,0 +1,7 @@
+<?php
+	echo '<form id="myform">';
+	echo '<input type="radio" id="name1" name="candidate" value="saurabh">saurabh</input><br />';
+	echo '<input type="radio" id="name1" name="candidate" value="sarah">sarah</input><br />';
+	echo '<input type="radio" id="name1"  name="candidate" value="shashank">shashank</input><br />';
+	echo '</form>';
+?>
